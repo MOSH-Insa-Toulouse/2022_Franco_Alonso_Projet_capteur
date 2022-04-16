36b604ee5a9ac03c20d91cd3e0c42ced08fc207e
Edouard Franco <98837584+EdouardFranco@users.noreply.github.com>
2022-04-16 14:24:44 +0200

    Update Capteur_Graphene.ino

--- Capteur_Graphene/Capteur_Graphene.ino
+++ Capteur_Graphene/Capteur_Graphene.ino
@@ -16,6 +16,7 @@ float Rs2=0;
 volatile unsigned int encoder0Pos = 0;
 SoftwareSerial mySerial(rxPin ,txPin); //Définition du software serial
 
+float Vadc;
 
 // Pour le OLED
 #include <Adafruit_SSD1306.h>
@@ -27,7 +28,9 @@ SoftwareSerial mySerial(rxPin ,txPin); //Définition du software serial
 
 Adafruit_SSD1306 ecranOLED(nombreDePixelsEnLargeur, nombreDePixelsEnHauteur, &Wire, brocheResetOLED);
 
-void Affichage_OLED(float Rs){ 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void Affichage_OLED(float Rs, float Vadc){ 
   byte tailleDeCaractere=2;
   byte numeroDeLigne=1;
   boolean bCouleurInverse = false;
@@ -35,25 +38,28 @@ void Affichage_OLED(float Rs){
   ecranOLED.setTextSize(tailleDeCaractere);                   // Taille des caractères (1:1, puis 2:1, puis 3:1)
   ecranOLED.setCursor(0, 0);                                  // Déplacement du curseur en position (0,0), c'est à dire dans l'angle supérieur gauche
   ecranOLED.setTextColor(SSD1306_WHITE);                  // Affichage du texte en "blanc" (avec la couleur principale, en fait, car l'écran monochrome peut être coloré)
+  ecranOLED.print("V= ");
+  ecranOLED.print(Vadc);
+  ecranOLED.println(" V");
   ecranOLED.print("R= ");
   ecranOLED.print(Rs);
   bCouleurInverse = !bCouleurInverse;
   ecranOLED.display();                            // Transfert le buffer à l'écran
 
-    // ***********************************************************************
-    // Affichage de 8 lignes à couleurs alternées, dans chaque écran successif
-    // ***********************************************************************
+  // ***********************************************************************
+  // Affichage de 8 lignes à couleurs alternées, dans chaque écran successif
+  // ***********************************************************************
+  
   /*if(bCouleurInverse)
     ecranOLED.setTextColor(SSD1306_BLACK, SSD1306_WHITE);   // Couleur du texte, et couleur du fond
   else
     
   delay(500);*/
-  
 }
 
-//
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-float Rs;
+double Rs;
 int Vcc=5;
 int R1=100000;
 int R3=100000;
@@ -61,25 +67,23 @@ int R5=10000;
 int R2=1000;
 
 
-void Bluetooth_write(float Rs){
-  mySerial.println(Rs);
+void Bluetooth_write(double Rs){
+  double Res = Rs/10000;
+  mySerial.println(Res);
+  delay(500);
 }
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 void mesure(){
   // read the input on analog pin 0:
-  int sensorValue = analogRead(A0)/4;                   //on divise par 4 pour être sur une plage de 256 et non une plage de 1024 car l'analogRead donne une valeur entre 0 et 1023
+  double  sensorValue = analogRead(A0);                   //on divise par 4 pour être sur une plage de 256 et non une plage de 1024 car l'analogRead donne une valeur entre 0 et 1023
   
   // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
-  double Vadc = sensorValue * (5.0 / 1023.0);
-  
-  Rs = (1+R3/R2)*R1*(Vcc/Vadc)-R1-R5;
-  Serial.println(Rs); 
-  
-  /*if (value == rotation){
-    Bluetooth_write(Rs);  
-    Affichage_OLED(Rs);
-  }*/
+  Vadc = sensorValue * (5.0 / 1023.0);
+ 
+  Rs = ((1+R3/R2)*R1*(Vcc/Vadc))-R1-R5;
+  //Serial.println(Vadc); 
 }
 
 
@@ -120,52 +124,51 @@ void loop() {
         //mesure();
         Rs=Rs/1000;
         Bluetooth_write(Rs);  
-        Affichage_OLED(Rs);
-        Serial.println(Rs);
+        Affichage_OLED(Rs, Vadc);
+        //Serial.println(Rs);
         ecranOLED.println(" k");
         ecranOLED.display();
      }
       else if (RotPosition==-2){
         //mesure();
-        Serial.println(Rs/1000000);
+        //Serial.println(Rs/1000000);
         Bluetooth_write(Rs/1000000);
-        Affichage_OLED(Rs/1000000);
+        Affichage_OLED(Rs/1000000, Vadc);
         ecranOLED.println(" M");
         ecranOLED.display();
      }
      else if (RotPosition==0){
-        Serial.println(Rs);
+        //Serial.println(Rs);
       Bluetooth_write(Rs);
-      Affichage_OLED(Rs); 
+      Affichage_OLED(Rs, Vadc); 
      }
       else if (RotPosition==1){
         //mesure();
         Rs=Rs/1000;
         Bluetooth_write(Rs);  
-        Affichage_OLED(Rs);
-        Serial.println(Rs);
+        Affichage_OLED(Rs, Vadc);
+        //Serial.println(Rs);
         ecranOLED.println(" k");
         ecranOLED.display();
      }
       else if (RotPosition==2){
         //mesure();
-        Serial.println(Rs/1000000);
+       // Serial.println(Rs/1000000);
         Bluetooth_write(Rs/1000000);
-        Affichage_OLED(Rs/1000000);
+        Affichage_OLED(Rs/1000000, Vadc);
         ecranOLED.println(" M");
         ecranOLED.display();
      }
      else{
       //mesure();
-      Serial.println(Rs);
+      //Serial.println(Rs);
       Bluetooth_write(Rs);
-      Affichage_OLED(Rs);  
+      Affichage_OLED(Rs, Vadc);  
      }
-     Serial.print("Encoder RotPosition: ");
-     Serial.println(RotPosition);
-     }
-  
-
-     //Serial.println(Rs);
-     // this will print in the serial monitor.
- 
+     //Serial.print("Encoder RotPosition: ");
+     //Serial.println(RotPosition);
+     
+     Serial.println(Rs);
+     Serial.println(Vadc);
+     // this will print in the serial monitor.  
+}
